61a165748ad0a37a8ddfc977b564ae3d11716875
i-whammy <yas.iwami@gmail.com>
2022-01-04 09:37:43 +0900

    created endpoint for registering articles

--- src/blog_articles_importer/handler/ping.clj
+++ src/blog_articles_importer/handler/ping.clj
@@ -1,6 +1,5 @@
 (ns blog-articles-importer.handler.ping
-  (:require [ataraxy.core :as ataraxy]
-            [ataraxy.response :as response]
+  (:require [ataraxy.response :as response]
             [integrant.core :as ig]))
 
 (defmethod ig/init-key :blog-articles-importer.handler/ping [_ options]
---
+++ src/blog_articles_importer/handler/register.clj
@@ -0,0 +1,9 @@
+(ns blog-articles-importer.handler.register
+  (:require [integrant.core :as ig]
+            [ataraxy.response :as response]
+            [blog-articles-importer.usecase.uzabase :as uzabase]))
+
+(defmethod ig/init-key :blog-articles-importer.handler/register [_ options]
+  (fn [{[_] :ataraxy/result}]
+    (uzabase/execute)
+    [::response/ok "Succeeded."]))
