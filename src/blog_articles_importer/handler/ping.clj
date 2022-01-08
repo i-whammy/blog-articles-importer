9226610f7a555f8ca40ce54635b3c76c61ce0309
i-whammy <yas.iwami@gmail.com>
2022-01-08 21:26:59 +0900

    removed unnecessary arg

--- src/blog_articles_importer/handler/ping.clj
+++ src/blog_articles_importer/handler/ping.clj
@@ -2,6 +2,6 @@
   (:require [ataraxy.response :as response]
             [integrant.core :as ig]))
 
-(defmethod ig/init-key :blog-articles-importer.handler/ping [_ options]
+(defmethod ig/init-key :blog-articles-importer.handler/ping [_ _]
   (fn [{[_] :ataraxy/result}]
     [::response/ok "pong"]))
