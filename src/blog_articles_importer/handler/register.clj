8d76cbb0738b30833dd6bb5b4ec30976a632f097
i-whammy <yas.iwami@gmail.com>
2022-01-14 22:32:37 +0900

    wip sansan register

--- src/blog_articles_importer/handler/register.clj
+++ src/blog_articles_importer/handler/register.clj
@@ -6,4 +6,6 @@
 (defmethod ig/init-key :blog-articles-importer.handler/register [_ options]
   (fn [{[_ company] :ataraxy/result}]
     (let [company-register (get-in options [(keyword company) :register])]
-    [::response/ok (execute company-register)])))
+      (if (nil? company-register)
+        [::response/internal-server-error "No register found."]
+        [::response/ok (execute company-register)]))))
--- src/blog_articles_importer/usecase/register/sansan.clj
+++ src/blog_articles_importer/usecase/register/sansan.clj
@@ -1,11 +1,13 @@
 (ns blog-articles-importer.usecase.register.sansan
   (:require [blog-articles-importer.register :refer [Register]]
-            [integrant.core :as ig]))
+            [integrant.core :as ig]
+            [blog-articles-importer.usecase.register.uzabase :as uzabase]))
 
 (def ^:private base-url "https://buildersbox.corp-sansan.com/rss")
 (def ^:private company-name "株式会社Sansan")
 
-(defn register* [{:keys [article-boundary]}])
+(defn register* [options]
+  (uzabase/register* options))
 
 (defrecord SansanRegister
   [options]
