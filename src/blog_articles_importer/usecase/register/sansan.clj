0068244903e4d0819b48559130a9922cad0c00f8
i-whammy <yas.iwami@gmail.com>
2022-01-15 20:08:43 +0900

    wip sansan register

--- src/blog_articles_importer/usecase/register/sansan.clj
+++ src/blog_articles_importer/usecase/register/sansan.clj
@@ -1,13 +1,68 @@
 (ns blog-articles-importer.usecase.register.sansan
   (:require [blog-articles-importer.register :refer [Register]]
             [integrant.core :as ig]
-            [blog-articles-importer.usecase.register.uzabase :as uzabase]))
+            [blog-articles-importer.boundary.article :as boundary]
+            [clj-http.client :as http]
+            [net.cgrand.enlive-html :as html]))
 
 (def ^:private base-url "https://buildersbox.corp-sansan.com/rss")
 (def ^:private company-name "株式会社Sansan")
 
-(defn register* [options]
-  (uzabase/register* options))
+(defn- get-articles-body []
+  (-> (http/get base-url)
+      :body))
+
+(defn- extract [body]
+  (-> body
+      (html/html-snippet)
+      (html/select #{[:item :title] [:item :link] [:item :pubDate]})))
+
+(defn- transform [tuple]
+  (map (fn [[title link pubdate]]
+         (let [url "TODO"]
+           {:id (clojure.string/replace url #"[^0-9]" "")
+            :title (first (:content title))
+            :publish-date (first (:content pubdate))
+            :url url
+            :company-name company-name}))
+       tuple))
+
+(defn- ->articles-entity [content]
+  (->> (partition 3 content)
+       (transform)))
+
+(defn- ->iso-publish-date [publish-date]
+  (.format (java.time.OffsetDateTime/parse publish-date (java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME))
+           (java.time.format.DateTimeFormatter/ISO_LOCAL_DATE)))
+
+(defn- ->article-vec [{:keys [id title publish-date url company-name]}]
+  (conj []
+        id
+        title
+        (->iso-publish-date publish-date)
+        url
+        company-name))
+
+(defn- ->articles-vec [articles]
+  (reduce
+   (fn [acc article]
+     (conj acc (->article-vec article)))
+   []
+   articles))
+
+(defn- fetch []
+  (->> (get-articles-body)
+       (extract)
+       (->articles-entity)
+       (->articles-vec)))
+
+(defn- collect-registered-ids [returned-articles]
+  {:registered-ids (map :id returned-articles)})
+
+(defn register* [{:keys [article-boundary]}]
+  (->> (fetch)
+       (boundary/store article-boundary)
+       (collect-registered-ids)))
 
 (defrecord SansanRegister
   [options]
