91fbe42917860cf730501b46a62371ba9610cb29
i-whammy <yas.iwami@gmail.com>
2022-01-25 19:26:46 +0900

    refactor register of sansan

--- src/blog_articles_importer/register.clj
+++ src/blog_articles_importer/register.clj
@@ -19,8 +19,8 @@
 
 (defn ->iso-local-date
   [publish-date original-format]
-   (.format (java.time.OffsetDateTime/parse publish-date original-format)
-            (java.time.format.DateTimeFormatter/ISO_LOCAL_DATE)))
+  (.format (java.time.OffsetDateTime/parse publish-date original-format)
+           (java.time.format.DateTimeFormatter/ISO_LOCAL_DATE)))
 
 (defn ->article-id
   [url short-name]
@@ -41,6 +41,27 @@
        (conj acc (->article-vec date-formatted-article))))
    []
    articles))
+
+(defn- transform [tuple
+                  ;; fns
+                  {:keys [id-fn
+                          url-fn
+                          title-fn
+                          publish-date-fn]}
+                  ;; company
+                  {:keys [id
+                          short-name]}]
+  (map (fn [[title link publish-date]]
+         (let [url (url-fn link)]
+           {:id (id-fn url short-name)
+            :title (title-fn title)
+            :publish-date (publish-date-fn publish-date)
+            :url url
+            :company-id id})) tuple))
+
+(defn ->articles-entity [{:keys [tags partition-number fns company]}]
+  (-> (partition partition-number tags)
+      (transform fns company)))
 
 (defprotocol Register
   (execute [self company]))
--- src/blog_articles_importer/usecase/register/sansan.clj
+++ src/blog_articles_importer/usecase/register/sansan.clj
@@ -6,23 +6,17 @@
 
 (def ^:private base-url "https://buildersbox.corp-sansan.com/rss")
 
-(defn- transform [{:keys [id short-name]} tuple]
-  (map (fn [[title link pubdate]]
-         (let [url (first (:content link))]
-           {:id (register/->article-id url short-name)
-            :title (first (:content title))
-            :publish-date (first (:content pubdate))
-            :url url
-            :company-id id}))
-       tuple))
-
-(defn- ->articles-entity [company content]
-  (->> (partition 3 content)
-       (transform company)))
+(def ^:private fns {:id-fn (fn [url short-name] (str short-name (clojure.string/replace url #"[^0-9]" "")))
+                    :url-fn (fn [link] (first (:content link)))
+                    :title-fn (fn [title] (first (:content title)))
+                    :publish-date-fn (fn [publish-date] (first (:content publish-date)))})
 
 (defn- fetch [company]
-  (let [article-entity (->> (register/extract base-url #{[:item :title] [:item :link] [:item :pubDate]})
-                            (->articles-entity company))]
+  (let [tags (register/extract base-url #{[:item :title] [:item :link] [:item :pubDate]})
+        article-entity (register/->articles-entity {:tags tags
+                                                    :partition-number 3
+                                                    :fns fns
+                                                    :company company})]
     (register/->articles-vec article-entity (java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME))))
 
 (defn register* [{:keys [article-boundary company-boundary]} company-short-name]
