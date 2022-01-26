d22607a8e2112f93f27b062540ff26268c629ea4
i-whammy <yas.iwami@gmail.com>
2022-01-26 14:26:15 +0900

    register cyberagent

---
+++ src/blog_articles_importer/usecase/fetcher/cyberagent.clj
@@ -0,0 +1,35 @@
+(ns blog-articles-importer.usecase.fetcher.cyberagent
+  (:require [blog-articles-importer.fetcher :as fetcher]
+            [clojure.string :as s]
+            [integrant.core :as ig]))
+
+(def ^:private base-url "https://developers.cyberagent.co.jp/blog/archives/category/engineer/feed/")
+
+(defn- ->article-id [url short-name]
+  (let [slug (-> url
+                 (s/replace #"[^0-9]" ""))]
+  (str short-name slug)))
+
+(def ^:private fns {:id-fn ->article-id
+                    :url-fn (fn [link] (first (:content link)))
+                    :title-fn (fn [title] (first (:content title)))
+                    :publish-date-fn (fn [publish-date] (first (:content publish-date)))})
+
+(defn- fetch* [company]
+  (let [tags (fetcher/extract base-url #{[:item :title] [:item :link] [:item :pubDate]})
+        articles-entity (fetcher/->articles-entity {:tags tags
+                                                     :partition-number 3
+                                                     :fns fns
+                                                     :company company})]
+
+    (fetcher/->articles-vec
+     articles-entity
+     (java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME))))
+
+(defrecord CyberagentFetcher
+           [options]
+  fetcher/Fetcher
+  (execute [_ company] (fetch* company)))
+
+(defmethod ig/init-key :blog-articles-importer.usecase.fetcher/cyberagent [_ options]
+  (map->CyberagentFetcher options))
