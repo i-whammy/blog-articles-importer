66eb495beecd9af5d1f1bdd2c736553750f27ac6
i-whammy <yas.iwami@gmail.com>
2022-01-30 10:01:24 +0900

    added article entity validation

--- src/blog_articles_importer/fetcher.clj
+++ src/blog_articles_importer/fetcher.clj
@@ -3,7 +3,6 @@
             [net.cgrand.enlive-html :as html]
             [clojure.spec.alpha :as s]))
 
-
 (defn- get-articles-body [base-url]
   (-> (http/get base-url)
       :body))
@@ -23,6 +22,19 @@
   (.format (java.time.OffsetDateTime/parse publish-date original-format)
            (java.time.format.DateTimeFormatter/ISO_LOCAL_DATE)))
 
+(s/def :blog-articles-importer/iso-local-date #(re-matches #"[0-9]{4}-[0-9]{2}-[0-9]{2}" %))
+(s/def :blog-articles-importer.article/id string?)
+(s/def :blog-articles-importer.article/title string?)
+(s/def :blog-articles-importer.article/publish-date (s/and string?
+                                                           :blog-articles-importer/iso-local-date))
+(s/def :blog-articles-importer.article/url string?)
+(s/def :blog-articles-importer.article/company-id int?)
+(s/def :blog-articles-importer/article (s/and #(s/valid? :blog-articles-importer.article/id (:id %))
+                                              #(s/valid? :blog-articles-importer.article/title (:title %))
+                                              #(s/valid? :blog-articles-importer.article/publish-date (:publish-date %))
+                                              #(s/valid? :blog-articles-importer.article/url (:url %))
+                                              #(s/valid? :blog-articles-importer.article/company-id (:company-id %))))
+(s/def :blog-articles-importer/articles (s/coll-of #(s/valid? :blog-articles-importer/article %)))
 (defn- transform [tuple
                   ;; fns
                   {:keys [id-fn
@@ -32,6 +44,7 @@
                   ;; company
                   {:keys [id
                           short-name]}]
+  {:post [(s/valid? :blog-articles-importer/articles %)]}
   (map (fn [[title link publish-date]]
          (let [url (url-fn link)]
            {:id (id-fn url short-name)
@@ -40,8 +53,6 @@
             :url url
             :company-id id})) tuple))
 
-(s/fdef ->articles-entity
-        :ret seqable?)
 (defn ->articles-entity [{:keys [tags partition-number fns company]}]
   (-> (partition partition-number tags)
       (transform fns company)))
