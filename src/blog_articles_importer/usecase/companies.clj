ba773b2f2722d3fabf0e879baeab5015afb24c84
i-whammy <yas.iwami@gmail.com>
2022-02-15 08:57:36 +0900

    changed response

--- src/blog_articles_importer/usecase/companies.clj
+++ src/blog_articles_importer/usecase/companies.clj
@@ -2,6 +2,7 @@
   (:require [blog-articles-importer.boundary.company :as company-boundary]))
 
 (defn fetch [options]
-  (let [company-boundary (:company-boundary options)]
-    (->> (company-boundary/get-all company-boundary)
-         (map :short-name))))
+  (let [company-boundary (:company-boundary options)
+        companies (->> (company-boundary/get-all company-boundary)
+                       (map :short-name))]
+    {:companies companies}))
